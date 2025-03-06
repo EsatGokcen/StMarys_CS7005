8c910b5f92d09d39234f59168544fda2a3e685b8
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-06 16:49:33 +0000

    restored files

---
+++ StMarys_CS7005/week6/vite-project/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <small>
+            <p>Copyright 2025</p>
+            <p>This website is the legal property of Esat Gokcen</p>
+        </small>
+    )
+}
+
+export default Footer;
