8d7370c989321aa751004c0110452af72b0ccf3a
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-13 16:27:59 +0000

    added route elements to app jsx for about and servies

--- week6+week7/vite-project/src/App.jsx
+++ week6+week7/vite-project/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from './Home'
+import About from './About'
+import Services from './Services'
 import Contact from './Contact'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
   <BrowserRouter>
     <Routes>
       <Route index path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>
