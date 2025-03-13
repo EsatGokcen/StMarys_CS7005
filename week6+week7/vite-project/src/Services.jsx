926d7fc919a7a775a64ee30b22135d285145ac40
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-13 16:24:34 +0000

    new services jsx file for react

---
+++ week6+week7/vite-project/src/Services.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function Services() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
