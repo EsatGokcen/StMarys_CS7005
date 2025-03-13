8620153c5dfa025cc6a1c1e5edd14ee8c3d7d274
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-13 15:59:42 +0000

    added new footer with social media icons to react project

--- week6+week7/vite-project/src/components/Footer.jsx
+++ week6+week7/vite-project/src/components/Footer.jsx
@@ -1,8 +1,31 @@
 function Footer() {
     return (
         <small>
-            <p>Copyright 2025</p>
-            <p>This website is the legal property of Esat Gokcen</p>
+            <h3 class="FooterTitle">Contact Us</h3>
+            
+            <p class="FooterP">
+                If you would like to contact us, visit our 
+                <a href="contact.html" class="footer-link">Contact Us</a> page.
+            </p>
+        
+            <div class="footer-links">
+                <a href="privacy.html">Privacy Policy</a> |
+                <a href="terms.html">Terms of Service</a>
+            </div>
+        
+            <div class="footer-social">
+                <a href="https://facebook.com" target="_blank">
+                    <img src="assets/facebook_icon.png" alt="Facebook"/>
+                </a>
+                <a href="https://twitter.com" target="_blank">
+                    <img src="assets/twitter_icon.png" alt="Twitter"/>
+                </a>
+                <a href="https://instagram.com" target="_blank">
+                    <img src="assets/instagram_icon.png" alt="Instagram"/>
+                </a>
+            </div>
+        
+            <p class="FooterP">&copy; 2025 My React Website</p>
         </small>
     )
 }
