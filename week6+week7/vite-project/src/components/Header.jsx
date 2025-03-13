691c9050b8ad586da041077a65e852f948b4cb9a
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-13 15:56:34 +0000

    new header for vite project

--- week6+week7/vite-project/src/components/Header.jsx
+++ week6+week7/vite-project/src/components/Header.jsx
@@ -1,5 +1,19 @@
 function Header() {
-    return <h1>Hi</h1>
+    return (
+        <>
+            <h1 class="site-title">
+                <a href="index.html">My Website</a>
+            </h1>  
+            <nav role="Navigation">
+                <ul id="nav-list">
+                    <li><a href="index.html" class="active">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="gallery.html">Services</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </>
+    )
 }
 
 export default Header
