4b10b2b3532586c4271aff8df1355a313d85aa73
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-13 16:53:00 +0000

    fixed img src to use js variables to comply with React env

--- week6+week7/vite-project/src/components/Footer.jsx
+++ week6+week7/vite-project/src/components/Footer.jsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom'
+import facebookIcon from '../assets/facebook_icon.png'
+import twitterIcon from '../assets/twitter_icon.png'
+import instagramIcon from '../assets/instagram_icon.png'
 
 function Footer() {
     return (
@@ -16,14 +19,14 @@ function Footer() {
             </div>
         
             <div class="footer-social">
-                <a href="https://facebook.com" target="_blank">
-                    <img src="../../src/assets/facebook_icon.png" alt="Facebook"/>
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <img src={facebookIcon} alt="Facebook"/>
                 </a>
-                <a href="https://twitter.com" target="_blank">
-                    <img src="../../src/assets/twitter_icon.png" alt="Twitter"/>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                    <img src={twitterIcon} alt="Twitter"/>
                 </a>
-                <a href="https://instagram.com" target="_blank">
-                    <img src="../../src/assets/instagram_icon.png" alt="Instagram" />
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                    <img src={instagramIcon} alt="Instagram" />
                 </a>
             </div>
         
