22c82cdaf31a150763593e12898f6c84a0fd91b0
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-18 17:16:16 +0000

    added some html to home jsx file

--- week6+week7/vite-project/src/Home.jsx
+++ week6+week7/vite-project/src/Home.jsx
@@ -5,6 +5,20 @@ function Home() {
     return (
         <div className='page'>
             <Header />
+
+            <main className="home-content">
+                <section className="hero">
+                    <h1>Welcome to My Website</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Suspendisse varius enim in eros elementum tristique. Duis 
+                        cursus, mi quis viverra ornare, eros dolor interdum nulla, 
+                        ut commodo diam libero vitae erat.
+                    </p>
+                    <button className="cta-button">Learn More</button>
+                </section>
+            </main>
+
             <Footer />
         </div>
     )
