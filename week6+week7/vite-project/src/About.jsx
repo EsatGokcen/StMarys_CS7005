90bf598090502e02c6205eeeffc2ee969b722d1e
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-18 17:22:33 +0000

    some html for about section of vite project

--- week6+week7/vite-project/src/About.jsx
+++ week6+week7/vite-project/src/About.jsx
@@ -3,11 +3,34 @@ import Footer from "./components/Footer";
 
 function About() {
     return (
-        <div className='page'>
+        <div className="page">
             <Header />
+
+            <main className="about-content">
+                <section className="about-section">
+                    <h1>About Us</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Praesent commodo cursus magna, vel scelerisque nisl 
+                        consectetur et. Curabitur blandit tempus porttitor.
+                    </p>
+                    <p>
+                        Our mission is to provide top-quality services and create 
+                        innovative solutions for our clients. With a passionate team 
+                        and a commitment to excellence, we strive to make a 
+                        difference in everything we do.
+                    </p>
+                    <img 
+                        src="https://via.placeholder.com/600x300" 
+                        alt="About Us" 
+                        className="about-image"
+                    />
+                </section>
+            </main>
+
             <Footer />
         </div>
-    )
+    );
 }
 
 export default About
